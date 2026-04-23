42cc6234c3ea6e9f9b9e50335f960027e865a19e
brunocoronado49 <francorangelcoronado@gmail.com>
2026-04-23 11:04:36 -0600

    ♻️ Add new routes in "archivo de barril" for use user logic

--- src/presentation/index.ts
+++ src/presentation/index.ts
@@ -1,2 +1,7 @@
 export * from './server';
 export * from './routes';
+export * from './user/auth.middleware';
+export * from './user/auth.service';
+export * from './user/controller';
+export * from './user/email.service';
+export * from './user/routes';
